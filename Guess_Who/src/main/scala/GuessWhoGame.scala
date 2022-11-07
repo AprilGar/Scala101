05294201a1688aa9252f8d63379b5b3df6fe1bfc
April Garlington <april.garlington@hotmail.co.uk>
2022-11-07 14:36:05 +0000

    firstNameCorrect? method with passing tests

--- Guess_Who/src/main/scala/GuessWhoGame.scala
+++ Guess_Who/src/main/scala/GuessWhoGame.scala
@@ -9,20 +9,30 @@ class GuessWhoGame {
   val loki = Person("Loki", "male", "grey", "brown", false, false)
   val moose = Person("Moose", "male", "brown", "brown", true, true)
   val maple = Person("Maple", "female", "ginger", "brown", false, false)
-
   val personCards: Seq[Person] = Seq (zoe, russell, loki, moose, maple)
 
   val randomPerson = personCards(random.nextInt(personCards.length))
 
-  def wearsAHat(): Unit = {
+  def wearsAHat(): Seq[Person] = {
     personCards.filter {
       person => person.wearsHat == true
     }
   }
 
-  def wearsGlasses(): Unit = {
+  def wearsGlasses(): Seq[Person] = {
     personCards.filter {
       person => person.wearsGlasses == true
+    }
+  }
+
+  def firstNameCorrect(firstName: String, computerGuess: Person): String = {
+    if (firstName == computerGuess.firstName) {
+      "You're right! Woo!"
+    } else {
+      personCards.filter {
+        person => person.firstName != firstName
+      }
+      "Not correct, try again!"
     }
   }
 
--- Guess_Who/src/test/scala/GuessWhoTest.scala
+++ Guess_Who/src/test/scala/GuessWhoTest.scala
@@ -3,10 +3,12 @@ import org.scalatest.FlatSpec
 class GuessWhoTest extends FlatSpec{
 
   val GuessWhoGame = new GuessWhoGame
+  val computerGuess = Person("Loki", "male", "grey", "brown", false, false)
+  val testZoe = Person("Zoe", "female", "blonde", "grey", false, true)
+  val testPersonCards: Seq[Person] = Seq(testZoe, computerGuess)
 
   "GuessWhoGame" should "return a random person" in {
     assert(GuessWhoGame.randomPerson == GuessWhoGame.randomPerson)
-    println(GuessWhoGame.randomPerson.firstName)
   }
 
   "GuessWhoGame" should "return a filtered list of names for those who wearsHat is true" in {
@@ -17,7 +19,17 @@ class GuessWhoTest extends FlatSpec{
     assert(GuessWhoGame.wearsGlasses() == GuessWhoGame.wearsGlasses())
   }
 
+  "GuessWhoGame" should "return a string if guessed name is incorrect" in {
+    val guessedName = GuessWhoGame.firstNameCorrect("Zoe", computerGuess)
+    val incorrectGuess = "Not correct, try again!"
+    assert(guessedName == incorrectGuess)
+  }
 
+  "GuessWhoGame" should "return a string if guessed name is correct" in {
+    val guessedName = GuessWhoGame.firstNameCorrect("Loki", computerGuess)
+    val correctGuess = "You're right! Woo!"
+    assert(guessedName == correctGuess)
+  }
 
 
 }
